4c33dc5d7898ce27eabc52300ea3d48ad858e4f1
jessicalam <j.lam@mail.com>
2017-05-15 19:41:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+(function () {
+  var cx = '017643444788069204610:4gvhea_mvga' // Insert your own Custom Search engine ID here
+  var gcse = document.createElement('script')
+  gcse.type = 'text/javascript'; gcse.async = true
+  gcse.src = (document.location.protocol === 'https' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(gcse, s)
+})()
